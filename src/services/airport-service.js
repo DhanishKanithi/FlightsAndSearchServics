dd1f80c9f6c56902dd47825baa067bcf2596971f
Dhanish Kanithi <dhanishkanithi@gmail.com>
2024-09-29 16:30:35 +0530

    Added common crud classes for basic crud operations

--- src/services/airport-service.js
+++ src/services/airport-service.js
@@ -1,58 +1,10 @@
-const { AirportRepository } = require("../repositories/index");
+const CrudService = require('./crud-service');
+const {AirportRepository} = require('../repositories/index');
 
-class AirportService {
+class AirportService extends CrudService {
     constructor() {
-        this.AirportRepository = new AirportRepository();
-    }
-
-    async createAirport(data) {
-        try {
-            const airport = await this.AirportRepository.createAirport(data);
-            return airport;
-        }
-        catch (error) {
-            throw {error};
-        }
-    }
-
-    async deleteAirport(airportId) {
-        try{
-            const response = await this.AirportRepository.deleteAirport(airportId);
-            return response;
-        }
-        catch(error) {
-            throw {error}; 
-        }
-    }
-
-    async updateAirport(airportId, data) {
-        try {
-            const airport = await this.AirportRepository.updateAirport(airportId, data);
-            return airport;
-        }
-        catch (error) {
-            throw {error};
-        }
-    }
-
-    async getAirport(airportId) {
-        try {
-            const airport = await this.AirportRepository.getAirport(airportId);
-            return airport;
-        }
-        catch (error) {
-            throw {error};
-        }
-    }
-
-    async getAllAirports(filter) {
-        try {
-            const airports = await this.AirportRepository.getAllAirports({name : filter.name});
-            return airports;
-        }
-        catch(error) {
-            throw {error};
-        }
+        const airportRepository = new AirportRepository();
+        super(airportRepository);
     }
 }
 
